406d2c9fbb7b630419b706272c5558a9e6cba55f
Alexito <103476365+Alexito-Hub@users.noreply.github.com>
2023-07-26 21:35:05 -0500

    Create config.js

---
+++ config.js
@@ -0,0 +1,22 @@
+const fs = require('fs')
+
+const prefix = [
+    '.'
+]
+
+const owner = {
+    number: '51968374620@s.whatsapp.net',
+    name: 'Alessandro',
+    email: 'alessandrovillogas@outlook.es',
+    location: 'Huancayo, PE',
+    avatar_url: 'https://telegra.ph/file/d6fd8fbc2f7cdd01d77c1.jpg',
+    profile_url: 'underfined',
+    last_seen: '2023-07-21 10:30:00',
+    online_status: 'Online',
+};
+
+
+module.exports = {
+    prefix,
+    owner
+}
