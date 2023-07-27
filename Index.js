0f4f8eb19f8eaa5c9683e4ea385d9eef5e67e012
Alexito <103476365+Alexito-Hub@users.noreply.github.com>
2023-07-27 13:42:23 -0500

    Update Index.js

--- Index.js
+++ Index.js
@@ -143,10 +143,10 @@ const start = async () => {
         const user = participant.split('@')[0];
     
         if (action === 'add') {
-          const welcomeMessage = `¡Hola ${user}! Bienvenido/a al grupo. ¡Esperamos que te diviertas y disfrutes tu estancia aquí! 🎉`;
+          const welcomeMessage = `¡Hola @${user.split`@`[0]}! Bienvenido/a al grupo. ¡Esperamos que te diviertas y disfrutes tu estancia aquí! 🎉`;
           client.sendMessage(groupId, {text:welcomeMessage});
         } else if (action === 'remove') {
-          const goodbyeMessage = `Adiós ${user}. Esperamos que hayas tenido una buena experiencia en el grupo. ¡Te echaremos de menos! 👋`;
+          const goodbyeMessage = `Adiós @${user.split`@`[0]}. Esperamos que hayas tenido una buena experiencia en el grupo. ¡Te echaremos de menos! 👋`;
           client.sendMessage(groupId, {text:goodbyeMessage});
         }
       }
